f18fa2aa59b31d3bf1223245fea058eb738b0a35
NeverwhereNowhere <manpinas14@gmail.com>
2022-05-15 11:18:40 -0300

    Corrijo el nombre de CreateUserForumTable

--- src/db/migrations/20220515135325-CreateUserForumTable.js
+++ src/db/migrations/20220515135325-CreateUserForumTable.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.createTable('passenger', {
+    await queryInterface.createTable('userforum', {
       id: {
         type: Sequelize.Datatypes.INTEGER,
         primaryKey: true,
