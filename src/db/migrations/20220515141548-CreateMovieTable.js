d862ff772ac79911c990dcae65b5bc3800f50950
Uriel1621 <uriel.fischman@gmail.com>
2022-05-23 11:59:46 -0300

    Corrijo la migracion de CreateMovie

--- src/db/migrations/20220515141548-CreateMovieTable.js
+++ src/db/migrations/20220515141548-CreateMovieTable.js
@@ -1,51 +1,46 @@
 'use strict';
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.createTable('movie', {
+    await queryInterface.createTable('movies', {
       id: {
-        type: Sequelize.Datatypes.INTEGER,
+        type: Sequelize.DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true
       },
 
       name: {
-        type : Sequelize.Datatypes.STRING(50),
+        type : Sequelize.DataTypes.STRING(50),
 
         allowNull: false
       },
 
       description: {
-        type : Sequelize.Datatypes.STRING(300),
+        type : Sequelize.DataTypes.STRING(300),
 
         allowNull: false
       },
 
       platform: {
         
-        type: Sequelize.Datatypes.ENUM('netflix','amazon','HBO'),
+        type: Sequelize.DataTypes.ENUM('netflix','amazon','HBO'),
 
-      },
-
-      forumId: {
-        type: Sequelize.Datatypes.INTEGER,
-        foreignKey: true
       },
 
       createdAt: {
-        type: Sequelize.Datatypes.DATE,
+        type: Sequelize.DataTypes.DATE,
 
-        defaultValue: Sequelize.Datatypes.NOW
+        defaultValue: Sequelize.DataTypes.NOW
       },
 
       updatedAt: {
-        type: Sequelize.Datatypes.DATE,
+        type: Sequelize.DataTypes.DATE,
 
-        defaultValue: Sequelize.Datatypes.NOW
+        defaultValue: Sequelize.DataTypes.NOW
       }
 
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('movie');
+    return queryInterface.dropTable('movies');
   }
 };
