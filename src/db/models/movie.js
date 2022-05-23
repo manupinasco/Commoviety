22da7951b1225abb59d592601e1270316354e7f4
Uriel1621 <uriel.fischman@gmail.com>
2022-05-23 12:37:24 -0300

    Agrego movie model

---
+++ src/db/models/movie.js
@@ -0,0 +1,51 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Movie extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Movie.init({
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    name: {
+      type : DataTypes.STRING(50),
+      allowNull: false
+    },
+
+    description: {
+      type : DataTypes.STRING(300),
+      allowNull: false
+    },
+
+    platform: {
+      type: DataTypes.ENUM('netflix','amazon','HBO'),
+    },
+
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    },
+
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    }
+  }, {
+    sequelize,
+    modelName: 'Movie',
+  });
+  return Movie;
+};
