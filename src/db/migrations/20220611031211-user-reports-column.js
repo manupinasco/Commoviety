8e3e17ce7807db03893e4e351f8bbe24520246e7
Uriel1621 <uriel.fischman@gmail.com>
2022-06-11 00:43:40 -0300

    Agrego migracion columna reports de users

---
+++ src/db/migrations/20220611031211-user-reports-column.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+     await queryInterface.addColumn('users', 'reports', { 
+       type: Sequelize.DataTypes.INTEGER,
+       defaultValue: 0
+      });
+     
+  },
+
+  async down (queryInterface, Sequelize) {
+     await queryInterface.removeColumn('users','reports');
+     
+  }
+};
