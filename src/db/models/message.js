6cbf9aaee2fcfdb4efe7385f79f26db677549648
Uriel1621 <uriel.fischman@gmail.com>
2022-06-11 00:38:55 -0300

    Agrego message model

---
+++ src/db/models/message.js
@@ -0,0 +1,56 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Message extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Message.belongsTo(models.User)
+      Message.belongsTo(models.Forum)
+    }
+  }
+  Message.init({
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    text: {
+      type : DataTypes.STRING(200),
+      allowNull: false
+    },
+
+    reports: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+
+    userId: {
+      type: DataTypes.INTEGER,
+    },
+
+    forumId: {
+      type: DataTypes.INTEGER,
+    },
+
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: Sequelize.DataTypes.NOW
+    },
+
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: Sequelize.DataTypes.NOW
+    }
+  }, {
+    sequelize,
+    modelName: 'Message',
+  });
+  return Message;
+};
