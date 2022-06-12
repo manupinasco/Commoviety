b79e5612508e172827cfb35e8454957a4cf1434f
Uriel1621 <uriel.fischman@gmail.com>
2022-06-11 23:23:16 -0300

    Se modifica el test de enviar mensaje

--- test/messageSendingTest.js
+++ test/messageSendingTest.js
@@ -4,24 +4,47 @@ const chaiFetch = require('chai-fetch');
 chai.use(chaiFetch);
 const {assert} = chai;
 
+async function banUser() {
+    await axios({
+        method: 'put',
+        url: 'http://localhost:4444/banUser',
+        data: { idMessage: 3 }
+    })
+}
 
 describe('Message sending in forum', () => {
 
-    it('Returns 201 if the message is sent to the forum correctly', (done) => {
+    it("Returns 201 if the message is sent to the forum correctly", (done) => {
         axios({
             method: 'post',
             url: 'http://localhost:4444/messageForum',
             data: {idMessage: 2, idForum: 2}
             
-        }
-        ).then((response) => {
+        })
+        .then(response => {
             assert.equal(response.status, 201)
             done()
-        }, (err) => {
+        })
+        .catch(err => {
             assert.equal(err.response.status, 201)
             done()
         })
     })
-
-
+    it("Returns 422 if the message hasn't been sent because the user has more than 4 reports", (done) => {
+        banUser()
+        axios({
+            method: 'post',
+            url: 'http://localhost:4444/messageForum',
+            data: {idMessage: 3, idForum: 2}
+        })
+        .then(response => {
+            assert.equal(response.status, 201)
+            done()
+        })
+        .catch(err => {
+            assert.equal(err.response.data.message, 'BANNED_USER')
+            done()
+        })
+       
+    }) 
 })
