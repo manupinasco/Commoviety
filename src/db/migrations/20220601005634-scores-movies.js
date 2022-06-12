87c213751befb1a9268ee2b41754a37e459698f1
NeverwhereNowhere <manpinas14@gmail.com>
2022-06-12 12:47:03 -0300

    Arreglo nombre de la columna de MovieId

--- src/db/migrations/20220601005634-scores-movies.js
+++ src/db/migrations/20220601005634-scores-movies.js
@@ -2,14 +2,14 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-     await queryInterface.addColumn('scores', 'movie_id', { 
+     await queryInterface.addColumn('scores', 'MovieId', { 
        type: Sequelize.DataTypes.INTEGER
       });
      
   },
 
   async down (queryInterface, Sequelize) {
-     await queryInterface.removeColumn('movie_id');
+     await queryInterface.removeColumn('MovieId');
      
   }
 };
