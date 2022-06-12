4371970f06532fec4c619504b75dcbee38ae1f90
NeverwhereNowhere <manpinas14@gmail.com>
2022-06-12 12:47:37 -0300

    Agrego migración de Lista

---
+++ src/db/migrations/20220610155128-CreateListTable.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('lists', {
+      id: {
+        type: Sequelize.DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+
+      name: {
+
+        type : Sequelize.DataTypes.STRING(60),
+
+        allowNull: false
+      },
+
+      idUser: {
+        type: Sequelize.DataTypes.INTEGER
+      },
+
+
+      createdAt: {
+        type: Sequelize.DataTypes.DATE,
+
+        defaultValue: Sequelize.DataTypes.NOW
+
+      },
+
+      updatedAt: {
+
+        type: Sequelize.DataTypes.DATE,
+
+        defaultValue: Sequelize.DataTypes.NOW
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('lists');
+  }
+};
