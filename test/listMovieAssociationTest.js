d4356f182f49184cbbe5b4f525e36ee572e3532f
NeverwhereNowhere <manpinas14@gmail.com>
2022-06-12 15:48:15 -0300

    Creo test de task 7

---
+++ test/listMovieAssociationTest.js
@@ -0,0 +1,41 @@
+const chai = require('chai');
+const axios = require('axios');
+const chaiFetch = require('chai-fetch');
+chai.use(chaiFetch);
+const {assert} = chai;
+
+
+describe('ListMovieAssociation', () => {
+
+    it('Returns 201 if List and Movie are associated correctly', (done) => {
+
+        axios({
+            method: 'post',
+            url: 'http://localhost:4444/listMovie',
+            data: {idList: 101, idMovie: 5}
+            
+        }
+        
+        ).then((response) => {
+            
+            assert.equal(response.status, 201)
+            done()
+        }, (err) => {
+            assert.equal(err.response.status, 422)
+            done()
+        })
+    })
+
+   it('Returns 422 if ListMovie exists', (done) => {
+        axios({
+            method : 'post',
+            url: 'http://localhost:4444/listMovie',
+            data: {idList: 101, idMovie: 5}
+        })
+        .catch(err => {
+            assert.equal(err.response.data.message, 'LISTMOVIE_ALREADY_EXISTS')
+            done()
+        })
+    })
+
+})
