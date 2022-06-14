cd58f9f40f0e2aaf7b40026d4ea3f38a57ccd8fb
Alex Birman <alexg.birman@gmail.com>
2022-06-14 18:04:10 -0300

    Se modifica método moviesTopPopularity

--- index.js
+++ index.js
@@ -2,51 +2,53 @@ const express = require('express');
 
 const app = express();
 
-const {User, Forum} = require('./src/db/models')
+const { User, Forum } = require('./src/db/models')
 
-const {Score} = require('./src/db/models/') 
-const {Movie} = require('./src/db/models')
+const { Score } = require('./src/db/models/')
+const { Movie } = require('./src/db/models')
+
+const { Op } = require("sequelize");
 
 
 app.listen(4444)
 
 
 
-app.get('/users', async function(req, res) {
-    
+app.get('/users', async function (req, res) {
+
     try {
-        let data = await User.findAll() 
+        let data = await User.findAll()
 
         res.send(data)
     }
-    catch(error) {
+    catch (error) {
         console.log(error)
     }
-    
-    
-    
+
+
+
 
 })
 
-app.get('/forums', async function(req, res) {
-    
-    
-    let q = {"id": 2}
+app.get('/forums', async function (req, res) {
+
+
+    let q = { "id": 2 }
 
     try {
         let data = await Forum.findAll({
             where: q/* ,
             limit: 20 */
-        }) 
+        })
 
         res.send(data)
     }
-    catch(error) {
+    catch (error) {
         console.log(error)
     }
-    
-    
-    
+
+
+
 
 })
 
@@ -57,11 +59,11 @@ app.get('/users/:id', async function (req, res) {
     res.send(data)
 })
 
-app.post('/usersForums/:idUser/:idForum', async function(req, res) {
+app.post('/usersForums/:idUser/:idForum', async function (req, res) {
     try {
         let user = await User.findByPk(req.params.idUser)
 
-        if(user.getForums().length < 25) {
+        if (user.getForums().length < 25) {
             console.warn("User " + req.params.idUser + " is already participating in 25 forums, which is the limit")
         }
         else {
@@ -70,7 +72,7 @@ app.post('/usersForums/:idUser/:idForum', async function(req, res) {
 
         }
     }
-    catch(error) {
+    catch (error) {
         console.log(error)
     }
 })
@@ -79,38 +81,50 @@ app.post('/usersForums/:idUser/:idForum', async function(req, res) {
 
 app.get('/scores', async function (req, res) {
     let data = await Score.findAll() //viene del Model de sequelize
-    
-    
+
+
     res.send(data)
 })
 
 app.get('/scores/:id', async function (req, res) {
     let data = await Score.findByPk(req.params.id)
-    
-    
+
+
     res.send(data)
 })
 
-app.listen(6001)
-
-app.get('/', function (req, res){
-    res.send('Hello')
-})
-
-app.get('/movies', async function (req, res){
+app.get('/movies', async function (req, res) {
     data = await Movie.findAll()
     res.send(data)
 })
 
-app.get('/movies/:id', async function (req, res){
+app.get('/movies/:id', async function (req, res) {
     data = await Movie.findByPk(req.params.id)
     res.send(data)
 })
 
- app.get('/moviesCreate', async function (req, res){
+app.get('/moviesCreate', async function (req, res) {
     Movie.create({
-        name : 'spiderman',
+        name: 'spiderman',
         description: 'lorem ipsum',
         platform: 'netflix'
     })
-}) 
+})
+
+app.get('/moviesTopPopularity/:quantity', async function (req, res) {
+    try {
+        movies = await Movie.findAll({
+            limit: Number(req.params.quantity),
+            order: [
+                ['quantScores', 'DESC']
+            ]
+        })
+        console.log(movies)
+        res.send(movies)
+    }
+    catch(error) {
+        res.status(422).json(error)
+    }
+    
+
+})
