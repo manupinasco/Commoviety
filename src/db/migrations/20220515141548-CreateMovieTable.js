b5eeeb1303ce18c145a85cd51fc76794b73ff2ad
Alex Birman <alexg.birman@gmail.com>
2022-06-14 20:51:04 -0300

    Corrección de migraciones

---
+++ src/db/migrations/20220515141548-CreateMovieTable.js
@@ -0,0 +1,46 @@
+'use strict';
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('movies', {
+      id: {
+        type: Sequelize.DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+
+      name: {
+        type : Sequelize.DataTypes.STRING(300),
+
+        allowNull: false
+      },
+
+      description: {
+        type : Sequelize.DataTypes.STRING(300),
+
+        allowNull: false
+      },
+
+      platform: {
+        
+        type: Sequelize.DataTypes.ENUM('netflix','amazon','HBO'),
+
+      },
+
+      createdAt: {
+        type: Sequelize.DataTypes.DATE,
+
+        defaultValue: Sequelize.DataTypes.NOW
+      },
+
+      updatedAt: {
+        type: Sequelize.DataTypes.DATE,
+
+        defaultValue: Sequelize.DataTypes.NOW
+      }
+
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('movies');
+  }
+};
