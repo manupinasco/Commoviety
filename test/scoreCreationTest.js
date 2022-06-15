53da6523effb06d299eda32fb9112be869b7349c
NeverwhereNowhere <manpinas14@gmail.com>
2022-06-15 02:09:42 -0300

    Agrego before y after al test

--- test/scoreCreationTest.js
+++ test/scoreCreationTest.js
@@ -9,6 +9,8 @@ describe('ScoreCreation', () => {
 
     let idUser
     let idMovie
+    let idUser2
+    let idMovie2
 
 
     before('Create User and Movie', () => {
@@ -19,12 +21,10 @@ describe('ScoreCreation', () => {
                 data: {name: 'Spiderman', description: 'lorem ipsum', platform: 'Netflix'}
                 
             }).then((response) => {
-                idMovie = response
+                idMovie = response.data.idMovie
                 assert.equal(response.status, 201)
-                done()
             }, (err) => {
                 assert.equal(err.response.status, 422)
-                done()
             })
         }
         let crearUser = async () => {
@@ -34,19 +34,42 @@ describe('ScoreCreation', () => {
                 data: {nickname: 'Charlie'}
                 
             }).then((response) => {
-                idUser = response
+                idUser = response.data.idUser
                 assert.equal(response.status, 201)
-                done()
             }, (err) => {
                 assert.equal(err.response.status, 422)
-                done()
             })
         }
-        return Promise.all([crearMovie, crearUser])
+        let crearMovie2 = async () => {
+            return axios({
+                method : 'post',
+                url: 'http://localhost:4444/movies',
+                data: {name: 'Coraline', description: 'lorem ipsum', platform: 'Netflix'}
+                
+            }).then((response) => {
+                idMovie2 = response.data.idMovie
+                assert.equal(response.status, 201)
+            }, (err) => {
+                assert.equal(err.response.status, 422)
+            })
+        }
+        let crearUser2 = async () => {
+            return axios({
+                method : 'post',
+                url: 'http://localhost:4444/users',
+                data: {nickname: 'Martin'}
+                
+            }).then((response) => {
+                idUser2 = response.data.idUser
+                assert.equal(response.status, 201)
+            }, (err) => {
+                assert.equal(err.response.status, 422)
+            })
+        }
+        return Promise.all([crearMovie, crearUser, crearMovie2, crearUser2].map(fn => fn()))
     })
 
     it('Returns 201 if the Score is saved and associated', (done) => {
-
         axios({
             method : 'post',
             url: 'http://localhost:4444/scoreUser',
@@ -80,11 +103,11 @@ describe('ScoreCreation', () => {
         })
     })
 
-    /* it('Returns 201 if the List is created and the Movie added', (done) => {
+    it('Returns 201 if the List is created and the Movie added', (done) => {
         axios({
             method : 'post',
             url: 'http://localhost:4444/scoreUser',
-            data: {idUser: 4, idMovie: 3, value: 2}
+            data: {idUser: idUser2, idMovie: idMovie2, value: 2}
         })
         .then((response) => {
             
@@ -100,7 +123,7 @@ describe('ScoreCreation', () => {
         axios({
             method : 'post',
             url: 'http://localhost:4444/scoreUser',
-            data: {idUser: 4, idMovie: 4, value: 2}
+            data: {idUser: idUser2, idMovie: idMovie2, value: 2}
         })
         .then((response) => {
             
@@ -110,7 +133,68 @@ describe('ScoreCreation', () => {
             assert.equal(err.response.status, 422)
             done()
         })
-    }) */
+    }) 
+
+    after('Delete User and Movie', () => {
+        let borrarMovie = async () => {
+            return axios({
+                method : 'delete',
+                url: 'http://localhost:4444/movies',
+                data: {id: idMovie}
+                
+            }).then((response) => {
+                assert.equal(response.status, 201)
+            }, (err) => {
+                assert.equal(err.response.status, 422)
+            })
+            
+            
+            
+        }
+        let borrarUser = async () => {
+            return axios({
+                method : 'delete',
+                url: 'http://localhost:4444/users',
+                data: {id: idUser}
+                
+            }).then((response) => {
+                assert.equal(response.status, 201)
+            }, (err) => {
+                assert.equal(err.response.status, 422)
+            })
+        }
+        let borrarMovie2 = async () => {
+            return axios({
+                method : 'delete',
+                url: 'http://localhost:4444/movies',
+                data: {id: idMovie2}
+                
+            }).then((response) => {
+
+                assert.equal(response.status, 201)
+            }, (err) => {
+                assert.equal(err.response.status, 422)
+            })
+        }
+        let borrarUser2 = async () => {
+            return axios({
+                method : 'delete',
+                url: 'http://localhost:4444/users',
+                data: {id: idUser2}
+                
+            }).then((response) => {
+
+                assert.equal(response.status, 201)
+            }, (err) => {
+                assert.equal(err.response.status, 422)
+            })
+        }
+        
+        return Promise.all([borrarMovie, borrarUser, borrarMovie2, borrarUser2].map(fn => fn()))
+
+        
+    })
+
 
     
 
