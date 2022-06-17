e1fb55bcdcf67e46e90f928c0265addf7f939004
Uriel1621 <uriel.fischman@gmail.com>
2022-06-16 21:58:24 -0300

    Mejoro test de eliminar mensaje

--- test/messageDeleteTest.js
+++ test/messageDeleteTest.js
@@ -11,22 +11,45 @@ describe('Message deleting', () => {
     let idMessage
 
     before(async () => {
-        return axios({
-            method: 'post',
-            url: 'http://localhost:4444/users',
-            data: { nickname: 'Mike' }
-        })
-        .then((response) => {
-            idUser = response.data.idUser
+        let createForum = async function () {
             return axios({
                 method: 'post',
-                url: 'http://localhost:4444/messages',
-                data: {idUser: idUser}
+                url: 'http://localhost:4444/forums',
             })
             .then((response) => {
+                    idForum = response.data.idForum
+            })
+        }
+
+        let createUser = async function () {
+            return axios({
+                method: 'post',
+                url: 'http://localhost:4444/users',
+                data: { nickname: 'Mike' }
+            })
+            .then((response) => {
+                    idUser = response.data.idUser
+            })
+        }
+
+        let associateUserForum = async function () { 
+            return axios({
+            method: 'post',
+            url: 'http://localhost:4444/usersForums',
+            data: { idUser: idUser, idForum: idForum }
+        })}
+
+        let sendMessage = async function () {
+            return axios({
+                method: 'post',
+                url: 'http://localhost:4444/messagesForums',
+                data: { idForum: idForum, idUser: idUser }
+            }).then((response) => {
                 idMessage = response.data.idMessage
             })
-        })
+        } 
+
+        return Promise.all([createForum, createUser].map(fn => fn())).then(associateUserForum).then(sendMessage)
     })
 
     it("Returns 422 if message hasn't been deleted because the message has less than 3 reports", async () => {
@@ -88,10 +111,28 @@ describe('Message deleting', () => {
     })
 
     after(() => {
-        return axios({
-            method : 'delete',
-            url: 'http://localhost:4444/users',
-            data: {id: idUser}
+        let deleteForum = (async () => {
+            return axios({
+                method: 'delete',
+                url: 'http://localhost:4444/forums',
+                data: { id: idForum }
+
+            })
         })
+
+        let deleteUser = (async () => {
+            return axios({
+                method: 'delete',
+                url: 'http://localhost:4444/users',
+                data: { id: idUser }
+            })
+        })
+
+        return axios({
+            method: 'delete',
+            url: 'http://localhost:4444/usersForums',
+            data: { idUser: idUser, idForum: idForum }
+        })
+        .then(() => { return Promise.all([deleteForum, deleteUser].map(fn => fn())) })
     })
 })
